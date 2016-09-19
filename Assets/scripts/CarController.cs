613d78ede4d03ff0182f3249278ab32b33c6d70a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-19 14:37:07 +0100

    Add speed sensitive steering

--- Assets/scripts/CarController.cs
+++ Assets/scripts/CarController.cs
@@ -4,21 +4,43 @@ using System.Collections;
 public class CarController : MonoBehaviour {
 
 	private Rigidbody2D rb;
-	private float speedForce = 10f;
-	private float torqueForce = 1f;
+	private float speedForce = 15f;
+	private float torqueForce = 200f;
+	private float driftFactorSticky = 0.65f;
+	private float driftFactorSlippy = 1f;
+	private float maxStickyVelocity = 2f;
+	private float minStickyVelocity = 1f;
 
 	private void Awake () {
 		rb = GetComponent<Rigidbody2D> ();
 	}
 
 	private void FixedUpdate () {
+
+		float driftFactor = driftFactorSticky;
+
+		if (RightVelocity().magnitude > maxStickyVelocity) {
+			driftFactor = driftFactorSlippy;
+		}
+
+		if (RightVelocity().magnitude < minStickyVelocity) {
+			driftFactor = driftFactorSticky;
+		}
+
+		rb.velocity = ForwardVelocity () + RightVelocity() * driftFactor;
+
+		Debug.Log (RightVelocity ().magnitude);
+
 		if (Input.GetButton ("Accelerate")) {
 			rb.AddForce (transform.up * speedForce);
 		}
 
-		rb.AddTorque(Input.GetAxisRaw("Horizontal") * -torqueForce);
+		float tf = Mathf.Lerp (0, torqueForce, rb.velocity.magnitude / 5);
 
-		rb.velocity = ForwardVelocity ();
+
+
+		rb.angularVelocity = Input.GetAxisRaw("Horizontal") * -tf;
+
 	}
 
 	private Vector2 ForwardVelocity () {
